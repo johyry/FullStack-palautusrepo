749a3384be8d28362dfda7589f405ba2dc4903c2
johyry <johannes.hyry@hotmail.com>
2019-03-07 11:54:17 +0200

    4.15-4.19

--- viikko4/app.js
+++ viikko4/app.js
@@ -3,10 +3,11 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 const middleware = require('./utils/middleware');
-
+const usersRouter = require('./controllers/users');
 const mongoose = require('mongoose');
 const config = require('./utils/config');
 const blogsRouter = require('./controllers/blogs');
+const loginRouter = require('./controllers/login');
 const logger = require('./utils/logger');
 
 const app = express();
@@ -31,6 +32,8 @@ morgan.token('body', req => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
--- viikko4/controllers/blogs.js
+++ viikko4/controllers/blogs.js
@@ -1,24 +1,50 @@
 const blogsRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
 const Blog = require('../models/blog');
+const User = require('../models/user');
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7);
+  }
+  return null;
+};
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user', { username: 1 });
   response.json(blogs);
 });
 
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body;
   console.log(body);
-  if (body.likes === undefined) body.likes = 0;
 
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes
-  });
+  const token = getTokenFrom(request);
+
   try {
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' });
+    }
+
+    const user = await User.findById(decodedToken.id);
+
+    if (body.likes === undefined) body.likes = 0;
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+      user: body.userId
+    });
+
     const result = await blog.save();
+    if (user !== null) {
+      user.blogs = user.blogs.concat(result._id);
+      await user.save();
+    }
     response.status(201).json(result);
   } catch (exception) {
     next(exception);
